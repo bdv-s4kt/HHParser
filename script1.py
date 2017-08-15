ba439f55150a556bfdb18a19daa83396be2e685e
Dmitry Buiko <dmitry.buiko@promwad.com>
2017-08-15 18:12:58 +0300

    New version

--- script1.py
+++ script1.py
@@ -1,122 +1,142 @@
 #!/usr/bin/env python3
 
-def processFile(fileName):
-    file = open(fileName, 'r')
-    data = file.read()
-    file.close()
-        
+def processFile(fileNames):
+    """
+    Using fileName opens this file,
+    and return a list of hands
+    """
+
+    data = ""
+
+    for file in fileNames:
+        f = open(file, 'r')
+        data += f.read()
+        f.close()
+
+    hands = []
+    
     beg = 0
     end = 0
+
     while (True):
         beg = data.find('PokerStars', end)
         if beg == -1:
             break
         end = data.find('***********', beg)
         if end is not -1:
-            listOfHands.append(data[beg:end])
+            hands.append(data[beg:end])
         else:
-            listOfHands.append(data[beg:])
+            hands.append(data[beg:])
             
-#--------------------------------------------------
-listOfHands = []
+    return hands
 
-processFile('file1.txt')
-processFile('file2.txt')
-processFile('file3.txt')
+def getSeats(header):
+    """
+    Return a map with info about players on each seat.
+    """
 
-print("Number of Hands Processed: " + str(len(listOfHands)))
-#---------------------------------------------------
+    seats = {}
 
-#print(listOfHands[3])
+    max6Seats = ['BU', 'SB', 'BB', 'EP', 'MP', 'CO']
 
-seat4 = 'Seat 4: ' # EP
-seat5 = 'Seat 5: ' # MP
-seat6 = 'Seat 6: ' # CO
-seat1 = 'Seat 1: ' # BU
-seat2 = 'Seat 2: ' # SB
-seat3 = 'Seat 3: ' # BB
+    pos_beg = header.find('Seat #') + 6
+    pos_end = header.find(' is the button')
+    BU_pos = int(header[pos_beg : pos_end])
 
-baseInfo = listOfHands[3][:listOfHands[3].find('*** HOLE CARDS ***')]
-#print('Base Info:\n' + baseInfo)
-
-baseInfoLines = baseInfo.split('\n')
-
-for i in range(len(baseInfoLines)):
-    if seat4 in baseInfoLines[i]:
-        nameEP = baseInfoLines[i][len(seat4):].split()[0]
-        stackEP = baseInfoLines[i][len(seat4):].split()[1][1:]
-    if seat5 in baseInfoLines[i]:
-        nameMP = baseInfoLines[i][len(seat5):].split()[0]
-        stackMP = baseInfoLines[i][len(seat5):].split()[1][1:]
-    if seat6 in baseInfoLines[i]:
-        nameCO = baseInfoLines[i][len(seat6):].split()[0]
-        stackCO = baseInfoLines[i][len(seat6):].split()[1][1:]
-    if seat1 in baseInfoLines[i]:
-        nameBU = baseInfoLines[i][len(seat1):].split()[0]
-        stackBU = baseInfoLines[i][len(seat1):].split()[1][1:]
-    if seat2 in baseInfoLines[i]:
-        nameSB = baseInfoLines[i][len(seat2):].split()[0]
-        stackSB = baseInfoLines[i][len(seat2):].split()[1][1:]
-    if seat3 in baseInfoLines[i]:
-        nameBB = baseInfoLines[i][len(seat3):].split()[0]
-        stackBB = baseInfoLines[i][len(seat3):].split()[1][1:]
-
-BB = 5
-
-#print("   EP: " + nameEP + ' ' * (20 - len(nameEP)) + stackEP + ' ' * (10 - len(stackEP)) + str(float(stackEP[1:]) * 100 / BB) + ' BB')
-#print("   MP: " + nameMP + ' ' * (20 - len(nameMP)) + stackMP + ' ' * (10 - len(stackMP)) + str(float(stackMP[1:]) * 100 / BB) + ' BB')
-#print("   CO: " + nameCO + ' ' * (20 - len(nameCO)) + stackCO + ' ' * (10 - len(stackCO)) + str(float(stackCO[1:]) * 100 / BB) + ' BB')
-#print("   BU: " + nameBU + ' ' * (20 - len(nameBU)) + stackBU + ' ' * (10 - len(stackBU)) + str(float(stackBU[1:]) * 100 / BB) + ' BB')
-#print("   SB: " + nameSB + ' ' * (20 - len(nameSB)) + stackSB + ' ' * (10 - len(stackSB)) + str(float(stackSB[1:]) * 100 / BB) + ' BB')
-#print("-> BB: " + nameBB + ' ' * (20 - len(nameBB)) + stackBB + ' ' * (10 - len(stackBB)) + str(float(stackBB[1:]) * 100 / BB) + ' BB')
-
-# ----------------------------------------------
-
-seats = {'EP': 'Seat 4',
-         'MP': 'Seat 5',
-         'CO': 'Seat 6',
-         'BU': 'Seat 1',
-         'SB': 'Seat 2',
-         'BB': 'Seat 3',}
-
-actions = []
-
-for seat in seats.keys():
-    
-    foldedHands = 0
-
-    for hand in listOfHands:
-
-        PFpos = hand.find('*** HOLE CARDS ***')
-        
-        baseHandInfo = hand[:PFpos]
-        #print('===============================')
-        #print(baseHandInfo)
-        namePos = baseHandInfo.find(seats[seat]) + len(seats[seat]) + 2
-        endName = baseHandInfo.find('($', namePos) - 1
-        
-        name = baseHandInfo[namePos:endName]
-        #print('-------------------------------')
-        #print('name: ' + name)
-
-        otherPos = hand.find('***', PFpos + 18)
-        preflop = hand[PFpos:otherPos]
-        #print('-------------------------------')
-        #print(preflop)
-        playerPos = preflop.find(name + ':')
-        if playerPos is -1:
+    #print('BU in the Seat number: %s' % BU_pos)
+    while (True):
+        pos_beg = header.find('Seat ', pos_end)
+        if pos_beg == -1:
             break
-        action = preflop[playerPos + len(name) + 2:].split()[0]
-        #print('action: ' + action)
 
-        if action == 'folds':
-            foldedHands += 1
-        if action not in actions:
-            actions.append(action)
+        pos_beg += 5
+        pos_end = header.find(':', pos_beg)
+        
+        num_seat = int(header[pos_beg : pos_end])
+
+        index = (6 + num_seat - BU_pos) % 6
+
+        pos_beg = pos_end + 2
+        pos_end = header.find('(', pos_beg)
+
+        player_name = header[pos_beg : pos_end - 1]
+
+        pos_beg = pos_end + 1
+        pos_end = header.find(' in chips', pos_beg)
+
+        player_stack = header[pos_beg : pos_end]
+
+        seats[player_name] = {'Pos': max6Seats[index], 'Stack': player_stack}
+        #seats[max6Seats[index]] = {'Name' : player_name, 'Stack': player_stack}
+        
     
-    numberOfHands = len(listOfHands)
-    playedHands = numberOfHands - foldedHands
+    #seats['BU'] = BU_pos
+    
+    return seats
 
-    print(seat + " playes " + str(playedHands) + "/" + str(numberOfHands) + " [" + str(100 * playedHands / numberOfHands) + ']')
+def parseHeaderInfo(header):
+    """
+    Return a structure that describes header of hand:
+    such info as hand #, table name, time of play and so on
+    """
 
-print(actions)
+    info = {}
+
+    pos_beg = header.find('#')
+    pos_end = header.find(':', pos_beg)
+
+    info['Hand #'] = header[pos_beg + 1 : pos_end]
+
+    pos_beg = pos_end + 3
+    pos_end = header.find('(', pos_beg)
+
+    info['Game'] = header[pos_beg : pos_end - 1]
+
+    pos_beg = pos_end + 1
+    pos_end = header.find(')', pos_beg)
+
+    info['Blinds'] = header[pos_beg : pos_end]
+
+    pos_beg = header.find('[', pos_end)
+    pos_end = header.find(' ', pos_beg)
+
+    info['Date'] = header[pos_beg  + 1 : pos_end]
+
+    pos_beg = pos_end + 1
+    pos_end = header.find(']', pos_beg)
+
+    info['Time'] = header[pos_beg : pos_end]
+
+    pos_beg = header.find('Table') + 6
+    pos_end = header.find('Seat') - 1
+
+    info['Table'] = header[pos_beg : pos_end]
+
+    info['Players'] = getSeats(header)
+    
+    return info
+
+def parseHand(hand):
+    """
+    Return a structure that holds an information about hand
+    which is getting from parser of hand,
+    and it is read to put to SQL
+    """
+
+    res = {}
+
+    header = hand[:hand.find('*** HOLE CARDS ***')]
+    res['Header Info'] = parseHeaderInfo(header)
+    
+    return res
+
+if __name__ == '__main__':
+    fileNames = ['file1.txt',
+                 'file2.txt']
+    hands = processFile(fileNames)
+    print("Processed: %s files, obtain %s hands."  % (len(fileNames), len(hands)))
+
+    x = 28
+    #print(hands[index])
+    print(parseHand(hands[x]))
+
